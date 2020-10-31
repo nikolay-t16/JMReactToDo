0d091d7a3d5ad2ee74c1860e8dc825c9e1f6e3f4
Nikolay Tereshchenko <nikolay.ter@gmail.com>
2020-11-01 02:31:32 +0300

    FooterComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import FooterComponent from './components/blocks/FooterComponent/FooterComponent';
 
 function App() {
   return (
@@ -45,21 +46,7 @@ function App() {
               </div>
             </li>
           </ul>
-          <footer className="footer">
-            <span className="todo-count">1 items left</span>
-            <ul className="filters">
-              <li>
-                <button className="selected">All</button>
-              </li>
-              <li>
-                <button>Active</button>
-              </li>
-              <li>
-                <button>Completed</button>
-              </li>
-            </ul>
-            <button className="clear-completed">Clear completed</button>
-          </footer>
+          <FooterComponent/>
         </section>
       </section>
   );
